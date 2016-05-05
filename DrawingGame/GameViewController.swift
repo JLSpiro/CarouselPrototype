963358181f91323132041c9d9adeb58401230304
Steven Brent <scbrent@gmail.com>
2016-05-04 22:04:19 -0400

    Added background music for testing. It works.

--- DrawingGame/GameViewController.swift
+++ DrawingGame/GameViewController.swift
@@ -12,18 +12,21 @@ import AVFoundation
 
 class GameViewController: UIViewController {
     
+    // guess what this is for ;)
     var bkgMusicPlayer = AVAudioPlayer()
 
     override func viewDidLoad() {
         
         super.viewDidLoad()
         
-        let pathToBkgMusic = NSBundle.mainBundle().pathForResource("crystal", ofType:"mp3")
+        // load up the background music file and start playing
+        let pathToBkgMusic = NSBundle.mainBundle().pathForResource("Crystal", ofType:"mp3")
         if let pathToBkgMusic = pathToBkgMusic {
             let bkgMusicURL = NSURL(fileURLWithPath: pathToBkgMusic)
             do {
                 try bkgMusicPlayer = AVAudioPlayer(contentsOfURL: bkgMusicURL)
                 bkgMusicPlayer.play()
+                bkgMusicPlayer.volume = 1
             } catch {
                 print("Error loading bkg music")
             }
